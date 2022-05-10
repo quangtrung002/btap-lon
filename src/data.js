4ba5aac64618bcc0182932ad06cde454a48c2c2b
unknown <trungbina3qo@gmail.com>
2022-05-10 17:11:41 +0700

    create project

---
+++ src/data.js
@@ -0,0 +1,102 @@
+const data = [
+  {
+    iphone : [
+      {
+        id :"",
+        name : "",
+        image : "",
+        title : "",
+        price : "",
+        button : ""
+      }
+    ]
+  },
+  {
+    ipad : [
+      {
+        id :"",
+        name : "",
+        image : "",
+        title : "",
+        price : "",
+        button : ""
+      }
+    ]
+  },
+  {
+    mac : [
+      {
+        id :"",
+        name : "",
+        image : "",
+        title : "",
+        price : "",
+        button : ""
+      }
+    ]
+  },
+  {
+    airpods : [
+      {
+        id :"",
+        name : "",
+        image : "",
+        title : "",
+        price : "",
+        button : ""
+      }
+    ]
+  },
+  {
+    watch : [
+      {
+        id :"",
+        name : "",
+        image : "",
+        title : "",
+        price : "",
+        button : ""
+      }
+    ]
+  },
+  {
+    accessory : [
+      {
+        id :"",
+        name : "",
+        image : "",
+        title : "",
+        price : "",
+        button : ""
+      }
+    ]
+  },
+  {
+    service : [
+      {
+        id :"",
+        name : "",
+        image : "",
+        title : "",
+        price : "",
+        button : ""
+      }
+    ]
+  },
+]
+
+const navbar = [
+  "iphone",
+  "ipad",
+  "mac",
+  "apple watch",
+  "âm thanh",
+  "phụ kiện",
+  "dịch vụ",
+  "khuyến mại",
+  "trả góp"
+]
+
+const footer = [
+  
+]
