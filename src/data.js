d0738a9c0b37ffb8ba79ab09bc4635451f8b8e3d
Hoai An <anhoai0310@gmail.com>
2022-05-16 22:49:40 +0700

    up data iphone

--- src/data.js
+++ src/data.js
@@ -1,35 +1,91 @@
 const data = [
   {
+    title : "iPhone",
+    button : "Xem tất cả >",
     iphone : [
       {
         id :"1",
+        name : "Iphone 13 Pro Max",
+        image : "https://s110.convertio.me/p/5UFSWWG8evo_SwlA7wdAUw/e081324d4d34cb540c26d91bfc0a70d6/iPhone-13-pro-max.jpg",
+        price : "29.290.000",
+        Capacity: ["128GB","256GB","512GB","1TB"],
+        color : ["Alpine Green","Sierra Blue","Graphite","Silver","Gold"]
+      },
+      {
+        id :"2",
         name : "Iphone 13",
-        image : "",
-        title : "",
-        price : "",
-        button : ""
+        image : "https://s165.convertio.me/p/Ua0SBhn3k_6-11PBqRVnLg/e081324d4d34cb540c26d91bfc0a70d6/iPhone-13.jpg",
+        price : "19.990.000",
+        Capacity: ["128GB","256GB","512GB"],
+        color : ["Alpine Green","Midnight","Starlight","Blue","(PRODUCT) RED","Pink"]
+      },
+      {
+        id :"3",
+        name : "Iphone 12",
+        image : "https://s135.convertio.me/p/N2olYZ-HNRaUrhiRbRCzRQ/e081324d4d34cb540c26d91bfc0a70d6/iPhone-12-xanh.jpg",
+        price : "16.190.000",
+        Capacity: ["64GB","128GB","256GB"],
+        color : ["White","Black","Blue","Green","Purple","(PRODUCT) RED"]
+      },
+      {
+        id :"4",
+        name : "Iphone 11",
+        image : "https://s164.convertio.me/p/VGLlzDEWjttFlrfa39uwPQ/e081324d4d34cb540c26d91bfc0a70d6/iPhone-11-Green.jpg",
+        price : "11.690.000",
+        Capacity: ["64GB","128GB","256GB"],
+        color : ["White","Black","Yellow","Green","Purple","(PRODUCT) RED"]
       },
     ]
   },
   {
+    title : "iPad",
+    button: "Xem tất cả iPad >",
     ipad : [
       {
-        id :"",
-        name : "",
-        image : "",
-        title : "",
-        price : "",
-        button : ""
-      }
+        id :"1",
+        name : "iPad gen 9 (2021)",
+        image : "https://s134.convertio.me/p/cZjHpgxXg21-XZvflC1Uog/e081324d4d34cb540c26d91bfc0a70d6/ipad-gen-9.jpg",
+        price : "8.990.000",
+        Capacity: ["64GB","256GB"],
+        color : ["Space Gray","Silver"],
+        Model : ["Wi-Fi","Wi-Fi + 4G"]
+      },
+      {
+        id :"2",
+        name : "iPad mini 6 (2021)",
+        image : "https://s165.convertio.me/p/BtYeFnRRNk5pH7gp4OchyQ/e081324d4d34cb540c26d91bfc0a70d6/ipad-mini-6.jpg",
+        price : "13.290.000",
+        Capacity: ["64GB","256GB"],
+        color : ["Starlight","Space Gray","Purple","Pink"],
+        Model : ["Wi-Fi","Wi-Fi + 5G"]
+      },
+      {
+        id :"3",
+        name : "iPad Pro 11 inch (M1, 2021)",
+        image : "https://cdn-dgcei.nitrocdn.com/QaFavQVnaqgHtiSsAelwGDKVguOuACXM/assets/static/optimized/rev-8a81e75/wp-content/uploads/2021/11/ipad-pro-m1-11-768x768-1.webp",
+        price : "19.950.000",
+        Capacity: ["128GB","256GB","512GB","1TB","2TB"],
+        color : ["Space Gray","Silver"],
+        Model : ["Wi-Fi","Wi-Fi + 5G"]
+      },
+      {
+        id :"4",
+        name : "iPad Pro 12.9 inch (M1, 2021)",
+        image : "https://cdn-dgcei.nitrocdn.com/QaFavQVnaqgHtiSsAelwGDKVguOuACXM/assets/static/optimized/rev-8a81e75/wp-content/uploads/2021/11/ipad-pro-m1.12.9-768x768-1.webp",
+        price : "24.990.000",
+        Capacity: ["128GB","256GB","512GB","1TB","2TB"],
+        color : ["Space Gray","Silver"],
+        Model : ["Wi-Fi","Wi-Fi + 5G"]
+      },
     ]
   },
   {
+    title : "MAC",
     mac : [
       {
         id :"",
         name : "",
         image : "",
-        title : "",
         price : "",
         button : ""
       }
