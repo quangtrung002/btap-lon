66ae354ebc3c017f28e6976d9de389dda77a86ee
unknown <trungbina3qo@gmail.com>
2022-06-13 08:00:47 +0700

    edit An's code

--- src/Component/page/pageItem/pageItem.js
+++ src/Component/page/pageItem/pageItem.js
@@ -94,66 +94,63 @@ function PageItem({ heading, pathImg, allitem, posts }) {
             </div>
 
             {/* Xem chi tiet may */}
-            <div className={clsx(style.cardAll, "row gap-3")}>
-              {posts.map((item) => (
-                <div className={clsx(style.cardofphone, "text-center")}>
-                  <div className="row g-lg-4 g-md-3">
-                    <Link
-                      to={`/${heading.toLowerCase() + "/" + item.id}`}
-                      className="col col-md-6 col-lg-3"
-                    >
-                      <div className={clsx(style.card, "card text-start")}>
-                        <img
-                          src={item.image}
-                          className={clsx(style.cardImg, "card-img-top py-4")}
-                          alt={item.id}
-                        />
-                        <div className={clsx(style.cartBody, "card-body px-4")}>
-                          <h5 className={clsx(style.cardTitle, "card-title")}>
-                            {item.name}
-                          </h5>
-                          <p className={clsx(style.cardPrice, "card-text")}>
-                            {item.price && item.oldprice ? (
-                              <>
-                                <del
-                                  className={clsx(style.cardOldPrice, "me-2")}
-                                >
-                                  {item.oldprice}
-                                  <span className={clsx(style.dong)}>đ</span>
-                                </del>
-                                <span className={clsx(style.cardNewPrice)}>
-                                  {item.price}
-                                  <span className={clsx(style.dong)}>đ</span>
-                                </span>
-                              </>
-                            ) : item.oldprice ? (
-                              <span className={clsx(style.cardOldPrice)}>
-                                {item.oldprice}
-                                <span className={clsx(style.dong)}>đ</span>
-                              </span>
-                            ) : (
-                              <span className={clsx(style.cardNewPrice)}>
-                                {item.price}
-                                <span className={clsx(style.dong)}>đ</span>
-                              </span>
-                            )}
-                          </p>
-                          {item.sale && (
-                            <div
-                              className={clsx(
-                                style.cardSale,
-                                style.firstLetter
-                              )}
-                            >{`giảm ${item.sale}`}</div>
-                          )}
-                        </div>
-                      </div>
-                    </Link>
-                  </div>
-                </div>
-              ))}
-            </div>
           </div>
+        </div>
+        <div className="row g-3">
+          {posts.map((item, index) => (
+            <Link
+              to={`/${heading.toLowerCase() + "/" + item.id}`}
+              className="col col-md-6 col-lg-3"
+              key={index}
+            >
+              <div className={clsx(style.card, "card text-start")}>
+                <img
+                  src={item.image}
+                  className={clsx(style.cardImg, "card-img-top py-4")}
+                  alt={item.id}
+                />
+                <div className={clsx(style.cartBody, "card-body px-4")}>
+                  <h5 className={clsx(style.cardTitle, "card-title")}>
+                    {item.name}
+                  </h5>
+                  <p className={clsx(style.cardPrice, "card-text")}>
+                    {item.price && item.oldprice ? (
+                      <>
+                        <del
+                          className={clsx(style.cardOldPrice, "me-2")}
+                        >
+                          {item.oldprice}
+                          <span className={clsx(style.dong)}>đ</span>
+                        </del>
+                        <span className={clsx(style.cardNewPrice)}>
+                          {item.price}
+                          <span className={clsx(style.dong)}>đ</span>
+                        </span>
+                      </>
+                    ) : item.oldprice ? (
+                      <span className={clsx(style.cardOldPrice)}>
+                        {item.oldprice}
+                        <span className={clsx(style.dong)}>đ</span>
+                      </span>
+                    ) : (
+                      <span className={clsx(style.cardNewPrice)}>
+                        {item.price}
+                        <span className={clsx(style.dong)}>đ</span>
+                      </span>
+                    )}
+                  </p>
+                  {item.sale && (
+                    <div
+                      className={clsx(
+                        style.cardSale,
+                        style.firstLetter
+                      )}
+                    >{`giảm ${item.sale}`}</div>
+                  )}
+                </div>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
