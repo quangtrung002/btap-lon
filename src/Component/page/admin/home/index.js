0e85b7b28e52a8d0e6785874183f0bd87e4afa60
Hoai An <anhoai0310@gmail.com>
2022-06-21 12:12:59 +0700

    update page chonmay and form seacrh

--- src/Component/page/admin/home/index.js
+++ src/Component/page/admin/home/index.js
@@ -1,39 +1,9 @@
 import React, { memo } from 'react'
-<<<<<<< HEAD
-// import { Bar } from "react-chartjs-2"
-function Home() {
-  // const data = {
-  //   labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  //   datasets: [
-  //     {
-  //       label: '# of votes',
-  //       data: [12, 19, 3, 5, 2, 3],
-  //       backgroundColor: [
-  //         'rgba(255, 99, 132, 0.2)',
-  //         'rgba(54, 162, 235, 0.2)',
-  //         'rgba(255, 206, 86, 0.2)',
-  //         'rgba(75, 192, 192, 0.2)',
-  //         'rgba(153, 102, 255, 0.2)',
-  //         'rgba(255, 159, 64, 0.2)',
-  //       ],
-  //       borderColor: [
-  //         'rgba(255, 99, 132, 1)',
-  //         'rgba(54, 162, 235, 1)',
-  //         'rgba(255, 206, 86, 1)',
-  //         'rgba(75, 192, 192, 1)',
-  //         'rgba(153, 102, 255, 1)',
-  //         'rgba(255, 159, 64, 1)',
-  //       ],
-  //       borderWidth: 1,
-  //     },]
-  // }
-=======
 import clsx from 'clsx'
 import style from "./home.module.scss"
 
 function Home() {
 
->>>>>>> 5b496765aa95effd456143ff041d19345b6333d6
   return (
     <div className={clsx(style.home, "py-3")}>
       <h1 className={clsx(style.heading, " mb-5 fs-1 text-center fw-bold text-uppercase")}>thống kê</h1>
--- src/Component/page/pageItem/pageItem.js
+++ src/Component/page/pageItem/pageItem.js
@@ -1,5 +1,4 @@
 import React, { memo, useEffect } from "react";
-
 import clsx from "clsx";
 import style from "./pageItem.module.scss";
 import { Link } from "react-router-dom";
@@ -215,6 +214,30 @@ function PageItem({
             </div>
           </span>
         </span>
+      </div>
+      <div className={clsx(style.search)}>
+        <div className="row">
+          <div className="col">
+            <h1 className={clsx(style.heading, "h3 text-center m-0 p-0")}>
+              đăng ký nhận tin từ ShopDunk
+            </h1>
+            <p className={clsx(style.paragragh, "text-center m-0 p-0")}>
+              thông tin sản phẩm mới nhất và chương trình khuyến mãi
+            </p>
+            <div className={clsx(style.form, "d-flex gap-1")}>
+              <input
+                type="text"
+                class="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-default"
+                placeholder="email của bạn"
+              />
+              <button type="button" class="btn btn-primary">
+                Đăng ký
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
